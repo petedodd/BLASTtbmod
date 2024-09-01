c6b3a6487661e28ae114ced6801ea23ef971bf34
PJ Dodd <p.j.dodd@sheffield.ac.uk>
2024-09-01 16:23:19 +0100

    plotters and parameter makers

---
+++ R/get.parms.R
@@ -0,0 +1,126 @@
+##' Create parameter object for model
+##'
+##' TODO
+##' @title Create parameter object
+##' @param start_year 
+##' @param years 
+##' @return list
+##' @author Pete Dodd
+##' @export
+##' @import data.table
+##' @import logitnorm
+get.parms <- function( start_year, years  ){
+
+  ########## Model dimensions & simulation parameters required for setup ############
+  patch_dims <- 7                        # number of patches = 3x3 grid     # Put in func
+  age_dims <- 3                          # Number of age groups             # put in func
+  HIV_dims <- 3                          # None, HIV, ART                   # put in func
+  dt <- 1/12                             #NOTE monthly timesteps hardcoded
+  tt <- as.double(seq(0, 12 * years)) # Vector of timesteps
+  sim_length <- length(tt)               # Length of simulation
+  offsetPops <- rep(1,patch_dims)
+
+  ## birthrates
+  birthrate_dat <- merge(
+    BLASTtbmod::MWI$B[
+          Year %in% start_year:(start_year + years),
+          list(Year, Births)
+        ],
+    BLASTtbmod::MWI$N[
+          Year %in% start_year:(start_year + years),
+          list(N = sum(PopTotal)),
+          by = Year
+        ],
+    by = "Year"
+  )
+  birthrate_dat[, birthrate := 1e3 * Births / N ]
+  births_int <- approx(birthrate_dat$birthrate, n = sim_length)$y
+
+  ## initial age frax
+  age.frax <- BLASTtbmod::MWI$N[Year==start_year,PopTotal] #NOTE requires ages in order
+  age.frax <- age.frax / sum(age.frax)
+
+  ## Get correct years for HIV
+  HIV_inc_1990_2021 <- BLASTtbmod::HIV_inc_1990_2021[ which( HIV_inc_1990_2021$Year %in% start_year:(start_year+years) ),]
+  HIV_int <- approx(BLASTtbmod::HIV_inc_1990_2021$Adult_incidence_1000_uninfected_est, n = sim_length)$y
+
+  ## Time varying ART initiation - interpolate
+  ART_int <- approx(BLASTtbmod::HIV_inc_1990_2021$ART_inc_est, n = sim_length)$y
+
+  ## Risk-modifier for TB infection based on HIV status
+  TB_HIV_mod <- c( 1, 10, 2 )
+
+  ## Background death rates by HIV index
+  ## set background death rate for ART same as no HIV
+  tiz <- tz <- seq(from = start_year, to = (start_year + years), by = dt) # Year timepoints
+  tiz <- round(tiz)
+  tiz <- as.integer(tiz) - start_year + 1 # which MU row to use
+
+  ## create the death rate data structures 
+  ## NOTE: m_in still placeholder
+  ## No longer interpolated - takes static value for corresponding year for each timestep
+  mu_noHIV_int <- m_in_int <- matrix(data = 0, ncol = sim_length, nrow = age_dims)
+  MU <- BLASTtbmod::MWI$omega[Year %in% start_year:(start_year + years),
+                              list(Year, AgeGrp, mu = omegaT - r)]
+  MU <- as.matrix(dcast(MU, Year ~ AgeGrp, value.var = "mu"))[, -1]  # mu per age group per year
+  for (i in 1:ncol(mu_noHIV_int)) {
+    for (j in 1:ncol(MU)) {
+      val <- MU[tiz[i], j]
+      mu_noHIV_int[j, i] <- ifelse(val > 0, val, 0) # net death/out
+      m_in_int[j, i] <- ifelse(val < 0, -val, 0) # net in-migration
+    }
+  }
+
+  ## TODO
+  ## mu_HIV_int <- matrix( data=NA, ncol = sim_length, nrow = age_dims )
+  ## for( i in 1:age_dims ){
+  ##   mu_noHIV_int[i,] <- approx( runif( years, min=0.01, max=0.07 ), n=sim_length )$y
+  ##   mu_HIV_int[i,] <- approx( runif( 30*age_dims, min=0.04, max=0.09 ), n=sim_length )$y
+  ## }
+  mu_ART_int <- mu_noHIV_int
+  mu_HIV_int <- mu_noHIV_int
+
+  ## Set up list to pass to model
+  parms <- list(
+    dt = dt,
+    tt = tt,
+    Pi = pi,
+    epsi = 0, # seasonality
+    sim_length = sim_length,
+    popinit = as.numeric(BLASTtbmod::blantyre$population),
+    patch_dims = patch_dims,
+    HIV_dims = HIV_dims,
+    age_dims = age_dims,
+    age_rate = c(1 / 15, 1 / 35, 1e-6),
+    agefracs = age.frax, #initial age fractions
+    ageMids = c(15 / 2, (15 + 50) / 2, 60),
+    births_int = births_int,
+    HIV_int = HIV_int,
+    ART_int = ART_int,
+    TB_HIV_mod = TB_HIV_mod,
+    mu_noHIV_int = mu_noHIV_int,
+    mu_HIV_int = mu_HIV_int,
+    mu_ART_int = mu_ART_int,
+    m_in_int = m_in_int,
+    IRR = rep(1, patch_dims),
+    MM = (diag(patch_dims) * 3 + 0.5) / 3, # mixing matrix
+    ## TODO hyperparms separate data object
+    progress_rate = 0.5, # Progression to clinical
+    regress_rate = 0.05, # regression to subclinical
+    beta = qlnorm(0.5, log(10), 0.75), # Effective contact rate - beta (bet)
+    v = qbeta(0.5, 20.7, 77.9), # Partial infection protection - v (v)
+    pLL = qlnorm(0.5, log(0.62), 0.068), # Stabilisation rate - pLL (arig)
+    pDf = qlnorm(0.5, -2.837, 0.32), # Fast progression rate - pDf (pv)
+    pDs = qlnorm(0.5, -6.89, 0.58), # Slow progression rate - pDs (eps)
+    pDr = qlnorm(0.5, -3.95, 0.27), # Relapse rate - pDr (rel)
+    cdr = logitnorm::qlogitnorm(0.5, 0, 0.3), # Case detection ratio - cdr
+    cdr_SC = 0.05, # Case detection ratio for subclinical disease  (PLACEHOLDER)
+    dur = qlnorm(0.5, 1.1, 0.2), # Duration untreated TB - dur
+    tfr = qbeta(0.5, 2.71, 87.55), # CFR treated TB - tfr (txf)
+    cfr = qbeta(0.5, 25.48, 33.78), # CFR untreated TB - cfr (cfrn)
+    # ari0 = rlnorm( patch_dims, log(0.05), 1 )
+    ari0 = qlnorm(0.5, log(2), 0.75) # Initial condition parameter - ?? (ari0)
+    # ari0 = 1
+  )
+  return(parms)
+}
---
+++ R/plotutils.R
@@ -0,0 +1,427 @@
+## ## some utilities for plotting
+
+##' Collect total populations as data table to be used by each plotter
+##'
+##' TODO
+##' @title Postprocess model output
+##' @param Y 
+##' @param chain_step_num 
+##' @param out_type 
+##' @return data.table
+##' @author Pete Dodd
+##' @import data.table
+##' @export
+extract.pops <- function(Y, chain_step_num = 1, out_type = "N") {
+  rownames(Y) <- BLASTtbmod::get_cols
+  selnmz <- grep(pattern = paste0(out_type, "\\["), rownames(Y), value = TRUE)
+  D <- data.table::as.data.table(Y[row.names(Y) %in% selnmz,
+    chain_step_num, ,
+    drop = TRUE
+  ])
+  D <- cbind(selnmz, D)
+  D <- data.table::transpose(D, make.names = "selnmz")
+  colnames(D) <- selnmz
+  D[, step := 1:nrow(D)]
+  D <- melt(D, id = "step")
+  D[, c("patch", "acatno", "hivno") := data.table::tstrsplit(variable, split = ",")]
+  D[, patch := as.integer(gsub(paste0(out_type, "\\["), "", patch))]
+  D[, acatno := as.integer(acatno)]
+  D[, hivno := as.integer(gsub("\\]", "", hivno))]
+  D[, age := BLASTtbmod::agz[acatno]]
+  D[, hiv := BLASTtbmod::hivz[hivno]]
+  D[, patch := paste("Patch", patch)]
+  D$age <- factor(D$age, levels = BLASTtbmod::agz)
+  D$hiv <- factor(D$hiv, levels = BLASTtbmod::hivz)
+  D[, c("acatno", "hivno") := NULL]
+  D
+}
+
+
+
+##' Collect total populations as data table to be used by each plotter
+##'
+##' TODO check chains or particles?
+##' @title Postprocess model outputs for multiple chains
+##' @param Y 
+##' @param n_chain_steps 
+##' @param out_type 
+##' @return data.table
+##' @author Pete Dodd
+##' @import data.table
+##' @export
+extract.pops.multi <- function( Y, n_chain_steps = 100, out_type = 'N' ){
+  ## catch
+  if( dim(Y)[2] < n_chain_steps ){
+    n_chain_steps <- dim(Y)[2]
+    print(paste0(
+      "Warning: Setting N chain steps to max (",
+      dim(Y)[2], ")"
+    ))
+  }
+  samp <- sample(dim(Y)[2], n_chain_steps, replace = FALSE)
+  ## Rearrange & sample desired output 
+  rownames(Y) <- BLASTtbmod::get_cols
+  D <- data.table::as.data.table( Y[, samp, ] )
+  selnmz <- grep( pattern = paste0( out_type, '\\[' ), D$V1 )
+  D <- D[ selnmz, ]
+  colnames(D) <- c('subcomp','chain_step','t',out_type )
+  ## columns for each index
+  D[ ,c('patch','acatno','hivno'):=data.table::tstrsplit(subcomp,split=',')]
+  D[ ,patch:=as.integer(gsub(paste0( out_type, '\\[' ),'',patch))]
+  D[ ,acatno:=as.integer(acatno)]
+  D[ ,hivno:=as.integer(gsub('\\]','',hivno))]
+  D[ ,age:=BLASTtbmod::agz[acatno]]
+  D[ ,hiv:=BLASTtbmod::hivz[hivno]]
+  D[ ,patch:=paste( 'Patch', patch)]
+  D$age <- factor(D$age,levels=BLASTtbmod::agz)
+  D$hiv <- factor(D$hiv,levels=BLASTtbmod::hivz)
+  D[, c('acatno', 'hivno'):=NULL]
+  D
+}
+
+
+
+##' Default median N per age group per year (not by patch)
+##'
+##' TODO
+##' @title Plot comparing total populations
+##' @param Y 
+##' @param n_chain_steps 
+##' @param start_year 
+##' @param years 
+##' @param eps 
+##' @param by_comp 
+##' @return ggplot2
+##' @author Pete Dodd
+##' @import data.table
+##' @import ggplot2
+##' @export
+plot_compare_demog <- function(Y,
+                               n_chain_steps = 100,
+                               start_year=2015,years=6,
+                               eps=0.25, #quantile to use
+                               by_comp="none"
+                               ){
+  if (!by_comp %in% c("age", "none")) stop("by_comp must be 'none' or 'age'!")
+  dt <- 1 / 12 # months
+  ## read MWI data
+  real_dat <- BLASTtbmod::MWI$N[Year %in% start_year:(start_year + years)]
+  real_dat[["age"]] <- real_dat$AgeGrp
+  real_dat[["mid"]] <- real_dat$PopTotal * 1000
+  real_dat[["lo"]] <- NA_real_
+  real_dat[["hi"]] <- NA_real_
+  tot_tmp_rl <- real_dat[Year == start_year, sum(mid)] # total population start year
+  real_dat$mid_scaled <- real_dat$mid / tot_tmp_rl
+  ## reshape simulation data
+  D <- extract.pops.multi( Y, n_chain_steps )
+  D[,'subcomp' := NULL ]
+  ## sum over HIV
+  D <- D[, list(tot_N = sum(N)), by = list(chain_step, t, age)]
+  ## Scale to total pop at start by chainstep/particle at time step 1:
+  tot_tmp <- D[t == 1, list(pop_start = sum(tot_N)), by = chain_step]
+  D <- merge(D, tot_tmp, by = "chain_step")
+  D$pop_scaled <- D$tot_N / D$pop_start
+  ## aggregate if necessary & summarize
+  if (by_comp == "age") {
+        aggD <- D[, list(
+          mid_scaled = median(pop_scaled),
+          lo = quantile(pop_scaled, eps),
+          hi = quantile(pop_scaled, 1 - eps)
+        ),
+        by = list(t, age)
+        ]
+  } else { #all ages
+    real_dat <- real_dat[, list(mid_scaled = sum(mid_scaled)), by = Year]
+    real_dat[, c("lo", "hi", "age") := list(NA_real_, NA_real_, "all")]
+    ## NOTE chained:
+    aggD <- D[, list(pop_scaled = sum(pop_scaled)), by = list(chain_step, t)
+              ][, list(
+      mid_scaled = median(pop_scaled),
+      lo = quantile(pop_scaled, eps),
+      hi = quantile(pop_scaled, 1 - eps)
+    ),
+    by = t
+    ]
+  }
+  aggD[, "Year" := (t - 1) * dt + start_year]
+  ## construct plot
+  p <- ggplot2::ggplot( aggD, aes( Year, y = mid_scaled, ymin = lo, ymax = hi)) +
+    ggplot2::geom_ribbon(alpha = 0.3) +
+    ggplot2::geom_line() +
+    ggplot2::facet_wrap(~age) +
+    ggplot2::geom_point(data = real_dat,col=2,shape = 16) +
+    ggplot2::ylab("Population by year (scaled at t=1)") +
+    ggplot2::xlab("Time") +
+    ggplot2::ggtitle("Population, median + 50% PI vs real data (red)")
+  p + ggplot2::expand_limits(y = c(0, NA))
+}
+
+
+## library(scales)
+## library(plyr)
+## library(dplyr)
+## library(ggh4x)
+
+
+
+
+## # Separate function to deal with aggregating rates
+## # Defaults to providing notifrates over patches only
+## # Currently will only do patch in line with current real data
+## # Looks like real data has hiv/art/age available too, so can do this later?
+## # Data md7 must be in env
+## plot.compare.noterate.agrgt <- function( Y, n_chain_steps = 100,
+##                                          agrgt_by = c( 'patch' )){
+  
+##   D1 <- extract.pops.multi( Y, n_chain_steps, out_type='N' )
+##   D1[,'subcomp' := NULL ]
+##   D2 <- extract.pops.multi( Y, n_chain_steps, out_type='notes' )
+##   D2[,'subcomp' := NULL]
+##   D <- merge( D1, D2, by = c( 'chain_step', 't', 'patch', 'age', 'hiv'))
+
+##   agrgt_by <- c( agrgt_by, 'chain_step', 't' )
+##   aggD <- D %>%
+##     group_by(across(all_of(agrgt_by))) %>%
+##     summarise( tot_N = sum(N),
+##                tot_notes = sum(notes)) %>%
+##     as.data.table()
+##   aggD[, noterate := tot_notes/tot_N*1e5 ]
+  
+##   # Generate ribbon
+##   eps = 0.25
+##   aggD <- aggD[ ,.( mid=median( noterate ),
+##                lo = quantile( noterate, eps ),
+##                hi = quantile( noterate, 1-eps )),
+##             by=.(t, patch )]
+  
+##   real_dat = md7
+##   real_dat[[ 'patch' ]] <- paste( 'Patch', md7$comid )
+  
+##   p <- ggplot( aggD, aes( t, y = mid, ymin = lo, ymax = hi)) +
+##     geom_ribbon(alpha = 0.3) +
+##     geom_line() +
+##     facet_wrap(~patch) +
+##     geom_point(data = real_dat, col = 2, shape = 1) +
+##     ylab("Notification rate per month") +
+##     xlab("Month") +
+##     ggtitle("Real data, median + 50% PI")
+##   plot.new()
+##   print(p)
+
+## }
+
+
+
+
+
+## # general plotter - takes state from mcstate
+## # Either pmcmc_run$trajectories$state object or forecast$state object or filter.stocmmodr$history()
+## # will plot any output (out_type) by patch, age and/or hiv 
+## # (specified as character vector by_comp). Choose any chain_step.
+## # Defaults to chain_step 1, total population, aggregates over compartments
+## plot.pops.dynamic <- function( Y, chain_step_num=1, out_type='N',
+##                        by_comp=NULL ){
+
+##   D <- extract.pops( Y, chain_step_num, out_type )
+##   grp_vars <- c( 'step', by_comp )
+##   Dplot <- D %>% 
+##     group_by( across( all_of( grp_vars ))) %>%
+##     summarise( tot_pop = sum(value))
+##   Dplot$default_col <- 1
+  
+##   if( 'age' %in% by_comp ){
+##     by_comp <- by_comp[-which(by_comp=='age')]
+##     col_age <- T
+##   } else {
+##     col_age <- F
+##   }
+##   out_lab <- out_type # default
+##   if( out_type == 'N' ){
+##     out_lab <- 'Total population'
+##   } else {
+##     if( out_type == 'incidence'){
+##       out_lab <- 'Incidence'
+##     } else {
+##       if( out_type == 'notifrate'){
+##         out_lab <- 'Notification rate'
+##       }
+##     }
+##   }
+  
+##   p <- ggplot( Dplot, aes( x=step*dt+start_year, y = tot_pop )) +
+##     xlab( 'Step' )+
+##     ylab( out_lab )+
+##     scale_y_continuous(label=comma)+
+##     guides(color = guide_legend(title = "Age group"))+ 
+##     theme_light()
+##   if( col_age ){
+##     p <- p + geom_line( aes( colour = age ))
+##   }else{
+##     p <- p + geom_line()
+##   }
+##   if( length( by_comp > 0)){
+##     p <- p + facet_grid2( by_comp, 
+##                           independent = TRUE,
+##                           scales='free')
+##   }
+##   print(p)
+## }
+
+
+## ## demographic snapshot (bar)
+## demo.snapshot <- function( Y, chain_step_num=1, out_type='N', timestep=NULL ){
+  
+##   D <- extract.pops( Y, chain_step_num, out_type )
+##   if( length(timestep > 0)){
+##     p <- ggplot( D[ step==timestep, ], aes(age,value))
+##   } else {
+##     p <- ggplot( D, aes( age, value))
+##   }
+##   out_lab <- out_type # default
+##   if( out_type == 'N' ){
+##     out_lab <- 'Total population'
+##   } else {
+##     if( out_type == 'incidence'){
+##       out_lab <- 'Incidence'
+##     } else {
+##       if( out_type == 'notifrate'){
+##         out_lab <- 'Notification rate'
+##       }
+##     }
+##   }
+  
+##   p <- p + geom_bar(stat='identity')+
+##     facet_wrap(~patch)+
+##     scale_y_continuous(label=comma)+
+##     theme_light()+
+##     theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+##     xlab('Age')+
+##     ylab(out_lab)
+##   print(p)
+## }
+
+
+## ## HIV/ART over time
+## HIV.dynamic.pc <- function( Y, chain_step_num=1, out_type='N', by_age = F){
+  
+##   D <- extract.pops( Y, chain_step_num, out_type )
+
+##   ## reformat
+##   if( by_age == FALSE ){
+##     X <- dcast( D,patch+step ~ hiv, fun.aggregate = sum )
+##     vars <- c('HIVpc','ARTpc','patch','step')
+##     id <- c('patch','step')
+##   } else {
+##     if( by_age == TRUE ){
+##       X <- dcast( D,patch+step+age ~ hiv, fun.aggregate = sum )
+##       id <- c('patch','step', 'age')
+##       vars <- c('HIVpc','ARTpc','patch','step','age')
+##     } else { stop('by_age must be TRUE or FALSE') }
+##   }
+##   X[,tot:= `HIV-`+`HIV+/ART-`+`HIV+/ART+`]
+##   X[,HIVpc:=(`HIV+/ART-`+`HIV+/ART+`)/tot]
+##   X[,ARTpc:=`HIV+/ART+`/(`HIV+/ART-`+`HIV+/ART+` + 1e-10)]
+##   X <- melt(X[,..vars],id=id)
+  
+##   out_lab <- out_type # default
+##   if( out_type == 'N' ){
+##     out_lab <- 'Population'
+##   } else {
+##     if( out_type == 'incidence'){
+##       out_lab <- 'Incidence'
+##     } else {
+##       if( out_type == 'notifrate'){
+##         out_lab <- 'Notification rate'
+##       }
+##     }
+##   }
+  
+##   ## plot
+##   wrap_vars <- id[-which(id=='step')]
+##   ggplot(X, aes(step*dt+start_year, value, col=variable))+
+##     geom_line()+
+##     facet_grid(wrap_vars)+
+##     scale_y_continuous(label=percent)+
+##     theme_light()+
+##     xlab('Year')+
+##     ylab( paste( out_lab, '(Proportion)'))+
+##     theme(legend.position = 'top',
+##           legend.title=element_blank())+
+##     scale_color_discrete( labels = c( 'HIV+/ART-', 'HIV+/ART+'))
+## }
+
+
+## ## HIV/ART by age
+## HIV.snapshot.pc <- function( Y, chain_step_num=1, out_type='N', timestep=NULL ){
+  
+##   D <- extract.pops( Y, chain_step_num, out_type )
+  
+##   ## reformat
+##   if( length( timestep > 0)){
+##     X <- dcast( D[ step==timestep, ], patch+age ~ hiv, fun.aggregate = sum)
+##   } else {
+##     X <- dcast( D, patch+age ~ hiv, fun.aggregate = sum )
+##   }
+##   X <- melt(X,id=c('patch','age'))
+##   # X$variable <- factor(X$variable,levels=hnmz,ordered = TRUE)
+  
+##   ## plot
+##   ggplot(X, aes( age, value, fill=variable))+
+##     geom_bar(stat='identity',position = position_fill())+
+##     facet_wrap(~patch)+
+##     scale_y_continuous(label=percent)+
+##     theme_linedraw()+
+##     theme(axis.text.x = element_text(angle = 45, hjust = 1),
+##           legend.position = 'top',legend.title=element_blank())+
+##     xlab('Age')+ylab('Population')
+## }
+
+
+## ## TB incidence by time
+## TB.dynamics <- function( Y, chain_step_num=1, out_type='incidence', separate=FALSE, by_age=F){
+  
+##   D <- extract.pops( Y, chain_step_num, out_type )
+##   by_comp <- 'patch'
+
+##   if( separate==FALSE ){
+##     D[,hiv:=ifelse(hiv=='HIV-','HIV-','HIV+')]
+##     D <- D[,.(value=sum(value)),by=.(step,patch,age,hiv)]
+##   }
+##   if( by_age==FALSE ){
+##     D <- D[,.(value=sum(value)),by=.(step,patch,hiv)]
+##   } else {
+##     by_comp <- c( by_comp, 'age' )
+##   }
+  
+##   ggplot(D, aes(step*dt+start_year, value, col=hiv))+
+##     geom_line()+
+##     facet_grid2( by_comp, 
+##                  independent = TRUE,
+##                  scales='free')+
+##     theme_light()+
+##     xlab('Year')+
+##     ylab('TB incidence')
+## }
+
+
+# # To take either pmcmc_run$trajectories$state object or forecast$state object or filter.stocmmodr$history()
+# # requires TBN to be in env
+# #  To complete if patch-specific real N available
+# plot.compare.dynamic <- function( Y, n_chain_steps = 100, out_type = 'N' ){
+#   
+#   D <- extract.pops.multi( Y, n_chain_steps, out_type )
+#   D[ ,'subcomp':= NULL ]
+#   
+#   if( out_type == 'N'){
+#     real_dat = TBN
+#     real_dat[[ 't' ]] <- real_dat$month
+#     real_dat[[ 'patch' ]] <- real_dat$zone
+#   }
+#   
+#   agrgt_by <- c( 'chain_step', 't', 'patch' )
+#   aggD <- D %>% 
+#     group_by(across(all_of(agrgt_by))) %>%
+#     summarise( tot_N = sum(N)) %>%
+#     as.data.table()
+#   
+# }
