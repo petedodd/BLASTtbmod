85e38cbd8ea8fc2533784f64f553c61a1afe4cf8
Pete Dodd <p.j.dodd@sheffield.ac.uk>
2026-05-31 16:12:47 +0100

    end arg for restart

--- R/get.parms.R
+++ R/get.parms.R
@@ -370,60 +370,16 @@ get.parms <- function(start_year,
 ##' @param parms Parameter list from previous runs, see 'get.parms()'
 ##' @param restart_step Step to restart at, in same units as 'tt' in parms
 ##' @param end_state Output from previous runs, in same format as output of 'stocm()' - should contain all timesteps up to and including restart_step
+##' @param sim_length How long is the new simulation? If NULL, will use length of 'tt' in parms minus restart_step
 ##' @return Updated parameter list with initial conditions and time-varying parameters updated to restart at given step
 ##' @author Pete Dodd
 ##' @export
 ##' @import data.table
-restart_parms <- function(parms, restart_step, end_state) {
+restart_parms <- function(parms, restart_step, end_state, sim_length = NULL) {
   cat("Creating new parameters to restart at given step...\n")
   ## gather snapshots for restart
   cat("Summarising end state...\n")
-  ## denom <- extract.pops.multi(end_state, dim(end_state)[2], out_type = "N")
-  ## numer <- extract.pops.multi(end_state, dim(end_state)[2], out_type = "D")
-  ## hnumr <- denom[t == restart_step,
-  ##   .(N = sum(N)),
-  ##   by = .(patch, age, hiv, particle = chain_step)
-  ## ]
-  ## denom <- denom[t == restart_step,
-  ##   .(N = sum(N)),
-  ##   by = .(patch, age, particle = chain_step)
-  ## ]
-  ## numer <- numer[t == restart_step,
-  ##   .(D = sum(D)),
-  ##   by = .(patch, age, particle = chain_step)
-  ## ]
-
-  ## ## TB prevalence by patch and age
-  ## both <- data.table::merge.data.table(
-  ##   denom, numer,
-  ##   by = c("patch", "age", "particle")
-  ## )
-  ## boths <- both[, .(prev = mean(D / N)), by = .(patch, age)]
-  ## D00 <- data.table::dcast(boths, patch ~ age, value.var = "prev")
-  ## D00 <- as.matrix(D00[, -1])
-
-  ## ## HIV state
-  ## hnumr[, tot := sum(N), by = .(patch, age, particle)]
-  ## hnumr[, p := N / tot]
-  ## hnumr <- hnumr[, .(p = mean(p)), by = .(patch, age, hiv)]
-  ## H00 <- array(hnumr[order(hiv, age, patch)]$p,
-  ##   c(7, 3, 3),
-  ##   dimnames = list(
-  ##     patch = unique(hnumr$patch),
-  ##     age = unique(hnumr$age),
-  ##     hiv = unique(hnumr$hiv)
-  ##   )
-  ## )
-
-  ## average over particles end
-  ## avstate <- apply(end_state, c(1,3), mean)
-  ## ## X0 <- rpois(n = length(avstate), lambda = avstate)
-  ## X0 <- round(avstate)
-
-  ## compute average at restart step
-  ## xinit <- apply(end_state[, ,restart_step], 1, mean)
-  ## xinit <- round(xinit)
-  xinit <- end_state[, 1,restart_step]
+  xinit <- end_state[, 1, restart_step]
   names(xinit) <- BLASTtbmod::get_cols
 
   ## grab the state variables for restart
@@ -433,6 +389,7 @@ restart_parms <- function(parms, restart_step, end_state) {
   stvrsn <- seq_len(length(BLASTtbmod::get_cols))[
     BLASTtbmod::get_cols %in% stvrs
   ]
+
   xinit <- xinit[stvrs]
   newpopinit <- parms$popinit
 
@@ -447,7 +404,12 @@ restart_parms <- function(parms, restart_step, end_state) {
   ## newparms$propinit_hiv <- H00
   ## newparms$popinit <- newpopinit
   ## timed items:
-  keep <- restart_step:length(parms$tt)
+  if (is.null(sim_length)) {
+    sim_end <- length(parms$tt)
+  } else {
+    sim_end <- restart_step + sim_length - 1
+  }
+  keep <- restart_step:sim_end ## length(parms$tt)
   newparms$tt <- newparms$tt[keep]
   newparms$tt <- newparms$tt - newparms$tt[1] # reset time to 0 at restart
   newparms$sim_length <- length(newparms$tt)
@@ -460,7 +422,6 @@ restart_parms <- function(parms, restart_step, end_state) {
   newparms$m_in_int <- newparms$m_in_int[, keep]
   newparms$ACFhaz0 <- newparms$ACFhaz0[, keep]
   newparms$ACFhaz1 <- newparms$ACFhaz1[, keep]
-
   return(newparms)
 }
 
